b924cc063f23a298b984678a9650060ed845e630
ksgr5566 <ksgr5566@gmail.com>
2022-10-18 18:46:25 +0530

    Added video bg

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -5,7 +5,7 @@ const Section = () => {
     return (
         <>
         <div className="w-full h-screen flex items-center">
-            <video src={videobg} autoPlay loop w-full h-full object-cover className="fixed -z-10" />
+            <video src={videobg} autoPlay loop w-full h-full object-cover playsinline className="fixed -z-10" />
             <div className='uppercase text-center text-black font-extrabold px-40'>
               <h3>Make it easy</h3>
               <h1 className='text-8xl '>FARMEASY</h1>
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,9 @@ module.exports = {
   ],
   theme: {
     extend: {},
+    screens: {
+      'md': '980px',
+    }
   },
   plugins: [],
 }
