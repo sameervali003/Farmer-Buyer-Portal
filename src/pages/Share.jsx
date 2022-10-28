d2dd6d5fa80df749b336c98acf05dc431af680d8
appala venkata avinash <aa760@snu.edu.in>
2022-10-28 18:44:04 +0530

    Modified Share page

--- src/pages/Share.jsx
+++ src/pages/Share.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useContext, useEffect } from "react";
 import axios from "axios";
 import { LoginContext } from "../contexts/LoginContext";
-
+import {states , s_a} from '../utils/cities'
 import ProductCard from "../components/ProductCard";
 
 const Share = () => {
@@ -12,6 +12,8 @@ const Share = () => {
   // const { account, loginStatus } = useContext(LoginContext);
   const [type, setType] = useState();
   const [prods, setProds] = useState([]);
+  const [myState, setMyState ] = useState("")
+  const [myCity, setMyCity ] = useState()
 
   useEffect(() => {
     axios
@@ -26,6 +28,8 @@ const Share = () => {
       });
   }, [type]);
 
+  
+
   const tools = [
     "All",
     "Tractors",
@@ -38,7 +42,7 @@ const Share = () => {
 
   return (
     <>
-      <div className="grid grid-cols-7  gap-2 mx-2 mb-10 mt-5">
+      <div className="grid grid-cols-7  gap-2 mx-2 mb-2 mt-5">
         {tools.map((tool, index) => (
           <button
             key={index}
@@ -49,7 +53,7 @@ const Share = () => {
           </button>
         ))}
       </div>
-
+       
       <div className="w-full sm:right-0 sm:w-20 bottom-0 my-10 fixed">
         <button
           className="w-full bg-gradient-to-r from-cyan-400 via-cyan-500 to-cyan-600 hover:bg-gradient-to-br text-white font-bold text-2xl py-2 rounded-full"
@@ -59,6 +63,36 @@ const Share = () => {
         </button>
       </div>
 
+<div className=" grid xs:grid-cols-2 grid-cols-1 gap-4 mx-2">
+<div>
+<label className="mt-4 font-mono block">State</label>
+<select className="border-2 border-gray-300 p-2 rounded-lg mt-4 w-full" defaultValue={myState} onChange={(e) => setMyState(e.target.value)}>
+                {
+                  states.map( (state, index)=>(
+                   <option key={index} value={state}> {state}</option>
+                     ))
+                }
+                </select>
+</div>
+<div>
+<label className="mt-4 font-sans block">My City</label>
+<select className="border-2 border-gray-300 p-2 rounded-lg mt-4 w-full" defaultValue={myCity} onChange={(e) => setMyCity(e.target.value)}>
+                { 
+                  s_a[states.indexOf(myState)].map( (city, index)=>(
+                   <option key={index} value={city}> {city}</option>
+                     ))
+                }
+                </select>
+</div>
+
+
+
+</div>
+
+<button type="button" className="block mx-auto text-white bg-gradient-to-br from-green-400 to-blue-600 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-green-200 mt-2 mb-4 py-2 px-4 rounded">
+  Filter
+</button>
+      
       <div class="px-2 pb-2 grid md:grid-cols-4 sm:grid-cols-3 grid-cols-2 gap-2 my-2 mx-2">
         {prods.map((prod, index) => (
           <ProductCard
